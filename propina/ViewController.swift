751713eb7e619bb78e203da72ad9b3b3e3fd0948
claudiacrg <47485665+claudiacrg@users.noreply.github.com>
2020-10-06 18:25:49 -0700

    Add files via upload

---
+++ propina/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  propina
+//
+//  Created by Alumno on 10/5/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    @IBOutlet weak var sldPorcentaje: UISlider!
+    
+    
+    @IBAction func doChangePorcentaje(_ sender: Any) {
+        lblPorcentaje.text = "\(Int(sldPorcentaje.value))%"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
